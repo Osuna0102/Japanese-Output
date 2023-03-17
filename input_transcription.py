f1e305d7d506073c7e37b39287561a52bb994d2b
Osuna0102 <osuna.alejo@gmail.com>
2023-03-16 23:18:27 -0500

    updated input so it ignores romaji and blankspaces

--- input_transcription.py
+++ input_transcription.py
@@ -1,3 +1,4 @@
+import os
 import speech_recognition as sr
 from datetime import datetime
 
@@ -9,14 +10,23 @@ mic = sr.Microphone()
 
 # get the current date and time for the filename
 now = datetime.now()
-current_date = now.strftime("%Y-%m-%d_%H-%M-%S")
+current_date = now.strftime("%Y-%m-%d")
 
-# create a new text file with the current date and time
-filename = f"{current_date}.txt"
-file = open(filename, "w", encoding="utf-8")
+# create a new text file with the current date
+filename = f"Transcritps\{current_date}.txt"
 
-# write the instructions to the file
-file.write("Speak now or press Ctrl-C to stop...\n\n")
+# check if the file already exists for the current day
+if os.path.isfile(filename):
+    file_mode = "a"
+else:
+    file_mode = "w"
+
+# open the file in append mode if it already exists, or in write mode if it doesn't
+file = open(filename, file_mode, encoding="utf-8")
+
+# write the instructions to the file if it's a new file
+if file_mode == "w":
+    file.write("Speak now or press Ctrl-C to stop...\n\n")
 
 try:
     while True:
@@ -34,7 +44,7 @@ try:
             print(f"You said: {text}\n")
 
             # write the recognized text to the file
-            file.write(f"You said: {text}\n\n")
+            file.write(f"{text}\n\n")
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
